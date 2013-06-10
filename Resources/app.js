3b3d28bc46cec246fb9373028e65a432a1d23808
Jens Roels <jens.roels@gmail.com>
2013-06-10 03:18:05 +0200

    Design row
    wat design voor de table row, moet enkel de buttons nog toevoegen

--- Resources/app.js
+++ Resources/app.js
@@ -6,6 +6,21 @@ var xhr = Titanium.Network.createHTTPClient();
 var my_lat;
 var my_lng;	
 
+var month=new Array();
+month[0]="JAN";
+month[1]="FEB";
+month[2]="MAR";
+month[3]="APR";
+month[4]="MAY";
+month[5]="JUN";
+month[6]="JUL";
+month[7]="AUG";
+month[8]="SEP";
+month[9]="OCT";
+month[10]="NOV";
+month[11]="DEC";
+
+
 //het home screen 
 var homeWindow = Titanium.UI.createWindow({
 	backgroundColor: "#ffffff"
@@ -24,6 +39,8 @@ var mapView = Titanium.Map.createView({
 
 var tableView = Titanium.UI.createTableView({
 	top:220,
+	backgroundColor:"#2e3945",
+	separatorColor:'#575f67',
 	data:data
 })
 
@@ -89,7 +106,7 @@ xhr.onload = function() {
    	title:title,
    	animate:true
    })
-   addRow(title);
+   addRow(title, jsonObject.events[i].start_time.toString(),jsonObject.events[i].location);
    }
    tableView.setData(data);
    mapView.addAnnotations(annotationObject);
@@ -97,25 +114,98 @@ xhr.onload = function() {
  
  
 //een rij toevoegen aan de array voor de tableview
-function addRow(titel){
+function addRow(titel,startdatum, location){
+	Ti.API.info(startdatum);
+	
 	var title= Titanium.UI.createLabel({
-   	text:titel,
-   	top:5,
+   	text:titel.substr(0,22).toUpperCase(),
+   	top:10,
    	color:'#ffffff',
-   	width:'300',
-   	left:65,
-   	font: { fontSize:16,fontWeight:'bold'},
+   	width:240,
+   	left:58,
+   	font: { fontSize:15,fontWeight:'bold'},
    	textAlign:'left'
    })
    
+   var dateView = Titanium.UI.createView({
+   	backgroundColor:"#3a4a58",
+   	width:49,
+   	height:"100%",
+   	left:0
+   })
+   
+   var dag = Titanium.UI.createLabel({
+   	text:startdatum.substr(8,2),
+   	font:{fontSize:20, fontWeight:"bold"},
+   	top: 15,
+   	left:12,
+   	color:"#fff"
+   })
+   
+   var maand = Titanium.UI.createLabel({
+   	text:month[parseInt(startdatum.substr(5,2), 10)],
+   	color:"#ffffff",
+   	top:45,
+   	left:9,
+   	font:{fontSize:15, fontWeight:"bold"}
+   })
+   
+   var line = Titanium.UI.createView({
+   	backgroundColor:"#ffffff",
+   	width:33,
+   	height:1,
+   	left:7,
+   	top: 41
+   })
+   
+   	var location = Titanium.UI.createLabel({
+ 	top:35,
+ 	left:72,
+ 	font:{fontSize:12, fontWeight:"bold"},
+ 	width:230,
+ 	textAlign:"left",
+ 	color:"#d0d1d3",
+ 	text:location.toUpperCase().substr(0,28)	
+ 	});
+ 	
+ 	var startuur= Titanium.UI.createLabel({
+   	text:startdatum.substr(11,5),
+   	color:'#d0d1d3',
+   	font: { fontSize:12, fontWeight:'bold'},
+   	width:212,
+   	top:55,
+   	left:72,
+   	textAlign:'left'
+   });
+   
+   var iconLocatie = Ti.UI.createImageView({
+  image:'/images/ico_locatie.png',
+  top:35,
+  left:55
+});
+   
+  var iconTijd = Ti.UI.createImageView({
+  image:'/images/ico_tijd.png',
+  top:55,
+  left:55
+});
+   
+   dateView.add(maand);
+   dateView.add(dag);
+   dateView.add(line);
    
     var row = Titanium.UI.createTableViewRow({
-   	height:60,
-   	backgroundColor:"#2e3945",
+   	height:80,
    	classname:"tableRow"
    })
    
+   
    row.add(title);
+   row.add(iconLocatie);
+   row.add(location);
+   row.add(iconTijd);
+   row.add(startuur);
+   row.add(dateView);
    data.push(row);
 }
 
